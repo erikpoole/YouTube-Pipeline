087576f8abb292c3b60d65ad09fc924a1a6ae7bd
Erik Poole <erikjpoole@gmail.com>
2020-03-17 19:32:47 -0600

    pulling files and running remove silence

--- pipeline.py
+++ pipeline.py
@@ -1,8 +1,11 @@
 import os
 import shutil
+import subprocess
 
 PENDING_PATH = "/mnt/c/Users/Mahkumazahn/Videos/Pending"
 TEMP_DIR = "./clips"
+
+# TODO: Make paths less nightmarish
 
 print("moving files from pending")
 os.mkdir(TEMP_DIR)
@@ -14,6 +17,11 @@ filepaths.sort(key=lambda filepath: os.path.getctime(filepath))
 for index, filepath in enumerate(filepaths):
     # TODO: use shutil.move when pipeline is complete
     shutil.copy(filepath, os.path.join(TEMP_DIR, str(index) + ".mp4"))
+
+print("removing silence")
+clip_names = os.listdir(TEMP_DIR)
+for clip_name in clip_names:
+    os.system("./video-remove-silence/video-remove-silence " + os.path.join(TEMP_DIR, clip_name))
 
 shutil.rmtree(TEMP_DIR)
 
