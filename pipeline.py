e77c709961e7ed07de1658dd4d51be71fcc66a5c
Erik Poole <erikjpoole@gmail.com>
2020-04-30 11:04:31 -0600

    capturing silence and converting to timestamps

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import time
 
 from moviepy.editor import VideoFileClip
 import numpy as np
@@ -16,8 +17,6 @@ AUDIO_THRESHOLD = .03           # stolen from jumpcutter, might need tweaking
 def main():
     # cleanup_last_run()
 
-    # moving files maybe not necessary if we're not actually removing space, 
-    # just creating xml
     # print("moving files from pending")
     # os.mkdir(CLIPS_DIR)
     # file_paths = copy_pending_files()
@@ -33,9 +32,26 @@ def main():
 
     print(find_silences("audio_test.mp4"))
 
+# moving files maybe not necessary if we're not actually editing files, just creating xml
+def copy_pending_files():
+    PENDING_PATH = "/mnt/c/Users/Mahkumazahn/Videos/Pending"
+
+    original_names = os.listdir(PENDING_PATH)
+    original_paths = [os.path.join(PENDING_PATH, original_name) for original_name in original_names]
+    original_paths.sort(key=lambda original_path: os.path.getctime(original_path))
+
+    new_paths = []
+    for index, original_path in enumerate(original_paths):
+        new_path = os.path.join(CLIPS_DIR, str(index) + ".mp4")
+
+        # TODO: use shutil.move when pipeline is complete
+        shutil.copy(original_path, new_path)
+        new_paths.append(new_path)
+
+    return new_paths
+
 def find_silences(path):
     # not handling channels
-    # verify audio fps is 44.1khz as opposed to 48khz, if not pass argument "audio_fps=48000"
     clip = VideoFileClip(path, audio_fps=AUDIO_FPS)
     print(clip.duration)
     print(clip.audio.fps)
@@ -45,7 +61,9 @@ def find_silences(path):
     audio_chunks = clip.audio.iter_chunks(chunk_duration=CHUNK_DURATION)
     silent_sections = []
     section_start = -1
+    num_sections = 0
     for index, chunk in enumerate(audio_chunks):
+        num_sections = num_sections + 1
         if section_start < 0 and is_silent(chunk):
             # start silent_section
             section_start = index
@@ -59,8 +77,8 @@ def find_silences(path):
             next
     
     # catch last section if empty
-    if section_start >= 0 and (len(tuple(audio_chunks)) - 1) - section_start >= SILENT_CHUNKS_REQUIRED:
-        add_silent_section(silent_sections, section_start, len(tuple(audio_chunks) - 1))
+    if section_start >= 0 and (num_sections - 1) - section_start >= SILENT_CHUNKS_REQUIRED:
+        add_silent_section(silent_sections, section_start, (num_sections - 1))
     
     return silent_sections
     
@@ -70,14 +88,30 @@ def is_silent(chunk):
     return True
 
 def add_silent_section(sections, start, end):
-    # Pads sounded sections with silence
-    sections.append([start + SILENCE_PADDING_CHUNKS, end - SILENCE_PADDING_CHUNKS])
+    # pads sounded sections with silence
+    starting_chunk = start + SILENCE_PADDING_CHUNKS
+    ending_chunk = end - SILENCE_PADDING_CHUNKS
 
+    starting_second = chunk_index_to_seconds(starting_chunk)
+    ending_second = chunk_index_to_seconds(ending_chunk)
+    starting_timestamp = seconds_to_timestamp(starting_second)
+    ending_timestamp = seconds_to_timestamp(ending_second)
+
+    sections.append([starting_timestamp, ending_timestamp])
+
+def chunk_index_to_seconds(index):
+    return index * CHUNK_DURATION
+
+def seconds_to_timestamp(seconds):
+    partial_seconds = seconds - int(seconds)
+    return "%s%.3f" % (time.strftime("%H:%M:%S", time.gmtime(seconds)), partial_seconds)
+    
 def get_element_from_template(name):
     path = "./xml_templates/" + name + ".xml"
     tree = et.parse(path, et.XMLParser(remove_blank_text=True))
     return tree.getroot()
 
+# needs rework to be add nodes for one video
 def add_video_nodes(tree, file_paths):
     video_playlist = tree.find(".//*[@id='video_playlist']")
     for index, path in enumerate(file_paths):
@@ -87,8 +121,7 @@ def add_video_nodes(tree, file_paths):
         entry = et.Element('entry')
         entry.attrib["producer"] = str(index)
         entry.attrib["in"] = "00:00:00.000"
-        # TODO Will need it's own method, not sure how this handles minutes
-        entry.attrib["out"] = "00:00:" + str(clip_length)
+        entry.attrib["out"] = seconds_to_timestamp(clip_length)
         video_playlist.append(entry)
         
         # add producer entry
@@ -108,37 +141,5 @@ def cleanup_last_run():
 
     if os.path.exists("./TEMP"):
         shutil.rmtree("./TEMP")
-
-def copy_pending_files():
-    PENDING_PATH = "/mnt/c/Users/Mahkumazahn/Videos/Pending"
-
-    original_names = os.listdir(PENDING_PATH)
-    original_paths = [os.path.join(PENDING_PATH, original_name) for original_name in original_names]
-    original_paths.sort(key=lambda original_path: os.path.getctime(original_path))
-
-    new_paths = []
-    for index, original_path in enumerate(original_paths):
-        new_path = os.path.join(CLIPS_DIR, str(index) + ".mp4")
-
-        # TODO: use shutil.move when pipeline is complete
-        shutil.copy(original_path, new_path)
-        new_paths.append(new_path)
-
-    return new_paths
-
-def remove_silence(file_paths):
-    for path in file_paths:
-        # both libraries dramatically lower size and bitrate... this is probably a problem
-        # shotcut videos are blank when the reference is inside wsl
-
-        # Increases audio volume
-        # os.system("python3 ./jumpcutter/jumpcutter.py --input_file %s --output_file %s --sounded_speed 1 --silent_speed 999999 --frame_margin 10 --sample_rate 48000" % (path, path.replace(".mp4", "_result.mp4")))
-
-        # Doesn't work with .flv files
-        os.system("./video-remove-silence/video-remove-silence " + path)
-
-        # os.remove(path)
-        # os.rename(path.replace(".mp4", "_result.mp4"), path)
-        break
 
 main()
