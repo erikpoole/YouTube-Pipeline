274f56853f1bda2947a76035249cc38e4d46afd0
Erik Poole <erikjpoole@gmail.com>
2020-05-03 14:27:05 -0600

    filters working, messy, not handling videos one silent section

--- pipeline.py
+++ pipeline.py
@@ -34,10 +34,27 @@ def main():
         noisy_sections = find_noisy_sections(path)
 
         print("adding %s noisy sections to mlt file" % get_filename(path))
-        producer = Producer(path, mlt_file.get_next_producer_id())
-        mlt_file.add_producer(producer)
+        if len(noisy_sections) < 2:
+            # handle addition of two filters to one clip
+            continue
+
+        first_section = noisy_sections.pop(0)
+        first_producer = Producer(path, mlt_file.get_next_producer_id())
+        first_producer.add_filter(mlt_file.get_next_filter_id(), "fade_in", first_section[0], first_section[0] + 1)
+        mlt_file.add_producer(first_producer)
+
+        middle_producer = Producer(path, mlt_file.get_next_producer_id())
+        mlt_file.add_producer(middle_producer)
+
+        last_section = noisy_sections.pop()
+        final_producer = Producer(path, mlt_file.get_next_producer_id())
+        final_producer.add_filter(mlt_file.get_next_filter_id(), "fade_out", last_section[1] - 1, last_section[1])
+        mlt_file.add_producer(final_producer)
+
+        mlt_file.add_playlist_entry(first_producer.id, first_section[0], first_section[1])
         for section in noisy_sections:
-            mlt_file.add_playlist_entry(producer.id, section[0], section[1])
+            mlt_file.add_playlist_entry(middle_producer.id, section[0], section[1])
+        mlt_file.add_playlist_entry(final_producer.id, last_section[0], last_section[1])
     
     mlt_file.write(os.path.join(output_path, date.today().strftime("%b-%d-%Y.mlt")))
 
@@ -71,12 +88,12 @@ def find_noisy_sections(path):
         if noisy_start < 0 and is_noisy(chunk):
             # if wasn't noisy and now is - set noisy_start
             noisy_start = index
-            next
+            continue
         if noisy_start >= 0 and not is_noisy(chunk):
             if noisy_end < 0:
                 # if was noisy and now isn't - set noisy_end
                 noisy_end = index
-                next
+                continue
             if index - noisy_end >= SILENT_CHUNKS_REQUIRED:
                 # if hasn't been noisy for SILENT_CHUNKS_REQUIRED, add noisy section
                 add_noisy_section(clip.duration, noisy_sections, noisy_start, noisy_end)
@@ -100,15 +117,13 @@ def add_noisy_section(seconds_upper_bound, sections, start, end):
     starting_second = chunk_index_to_seconds(starting_chunk)
     if starting_second < 0:
         starting_second = 0
-    starting_timestamp = seconds_to_timestamp(starting_second)
 
     ending_chunk = end + SILENCE_PADDING_CHUNKS
     ending_second = chunk_index_to_seconds(ending_chunk)
     if ending_second > seconds_upper_bound:
         ending_second = seconds_upper_bound
-    ending_timestamp = seconds_to_timestamp(ending_second)
 
-    sections.append([starting_timestamp, ending_timestamp])
+    sections.append([starting_second, ending_second])
 
 def chunk_index_to_seconds(index):
     return index * CHUNK_DURATION
@@ -147,13 +162,13 @@ class MLTFile:
         # producers must be inserted above playlists and tractor to satisfy shotcut
         self.root.insert(3 + self.producer_count, producer.root)
 
-    def add_playlist_entry(self, producer_number, starting_timestamp, ending_timestamp):
+    def add_playlist_entry(self, producer_number, starting_second, ending_second):
         video_playlist = self.root.find(".//*[@id='video_playlist']")
 
         entry = et.Element('entry')
         entry.attrib["producer"] = str(producer_number)
-        entry.attrib["in"] = starting_timestamp
-        entry.attrib["out"] = ending_timestamp
+        entry.attrib["in"] = seconds_to_timestamp(starting_second)
+        entry.attrib["out"] = seconds_to_timestamp(ending_second)
 
         video_playlist.append(entry)
 
@@ -177,5 +192,31 @@ class Producer:
         self.id = id
         self.duration = duration
         self.filename = filename
+
+    def add_filter(self, id, filter_type, starting_second, ending_second):
+        filter_root = get_element_from_template("filter")
+        filter_root.attrib["id"] = id
+        filter_root.attrib["in"] = seconds_to_timestamp(starting_second)
+        filter_root.attrib["out"] = seconds_to_timestamp(ending_second)
+
+        if filter_type == "fade_in":
+            self.__set_filter_to_fade_in(filter_root)
+        elif filter_type == "fade_out":
+            self.__set_filter_to_fade_out(filter_root)
+        else:
+            print("invalid filter selected")
+            return
+
+        self.root.append(filter_root)
+
+    def __set_filter_to_fade_in(self, filter_root):
+        filter_root.find(".//*[@name='level']").text = "0=0; 30=1"
+        filter_root.find(".//*[@name='mlt_service']").text = "brightness"
+        filter_root.find(".//*[@name='shotcut:filter']").text = "fadeInBrightness"
+
+    def __set_filter_to_fade_out(self, filter_root):
+        filter_root.find(".//*[@name='level']").text = "0=1; 30=0"
+        filter_root.find(".//*[@name='mlt_service']").text = "brightness"
+        filter_root.find(".//*[@name='shotcut:filter']").text = "fadeOutBrightness"
 
 main()
